527632855f5bd4061491de430e8e24d6a8489ed9
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-13 21:43:55 +0300

    Задача 38. Не смогла решить

---
+++ DZ_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+double[] RandomArray(int lenght, int minValue, int maxValue)
+{
+    double[] array = new double[lenght];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = RandomDouble(minValue, maxValue);
+        return array;
+    }
+}
+double[] array = RandomArray(5, 0, 10);
